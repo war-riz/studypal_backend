8c32c0001413bb4827d0de233627827cf74a3bee
war-riz <kehindewaris54@gmail.com>
2025-03-26 14:55:36 +0100

    changes to the model

--- studypal/models.py
+++ studypal/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     course = models.CharField(max_length=255, blank=True, null=True)  # Course of Study
     gender = models.CharField(
         max_length=10,
-        choices=[('Boy', 'Boy'), ('Girl', 'Girl')],
+        choices=[('Male', 'Male'), ('Female', 'Female')],
     )  # Gender Selection
     role = models.CharField(
         max_length=10,
